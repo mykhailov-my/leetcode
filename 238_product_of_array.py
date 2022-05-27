9ebd32ca42d12c1c534f8fad21bb35e114eeace1
Михайло <mykhailov.my@protonmail.com>
2022-05-27 23:43:08 +0300

    238

---
+++ 238_product_of_array.py
@@ -0,0 +1,14 @@
+# NOTE Did with help
+class Solution:
+    def productExceptSelf(self, nums: list[int]) -> list[int]:
+        p = 1
+        n = len(nums)
+        output = []
+        for i in range(0,n):
+            output.append(p)
+            p = p * nums[i]
+        p = 1
+        for i in range(n-1,-1,-1):
+            output[i] = output[i] * p
+            p = p * nums[i]
+        return output
