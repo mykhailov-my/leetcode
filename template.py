3b83242291d512dcaa99653fbb5342b768751fb6
Михайло <mykhailov.my@protonmail.com>
2022-06-20 20:11:45 +0300

    added template

---
+++ template.py
@@ -0,0 +1,23 @@
+"""
+Status -> (Solved by myself, Solved with hint, Solved on review, Implemented with solution)
+
+Time complexity ->
+Space complexity ->
+
+Solution
+1.
+2.
+3.
+"""
+
+class Solution:
+    def func(self, n):
+        pass
+
+
+
+if __name__ == '__main__':
+    n = ...
+    sol = Solution()
+    res = sol.func(n)
+    print(f'Result --> {res}')  
