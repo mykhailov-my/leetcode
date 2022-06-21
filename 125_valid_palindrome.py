c2f78e9cc84216fc8801d9edfe0597fb6ebb1652
Михайло <mykhailov.my@protonmail.com>
2022-06-21 19:08:48 +0300

    improve solution

--- 125_valid_palindrome.py
+++ 125_valid_palindrome.py
@@ -14,8 +14,27 @@ class Solution:
             end -= 1
         return True
 
+# Solved with hint
+# Time complexity O(N)
+# Space complexity O(1)
+class Solution2:
+    def isPalindrome(self, s: str) -> bool:
+        begin = 0
+        end = len(s) - 1
+        while begin < end:
+            while begin < end and not s[begin].isalnum():
+                begin += 1
+            while begin < end and not s[end].isalnum():
+                end -= 1
+            if s[begin].lower() != s[end].lower():
+                return False
+            begin += 1
+            end -= 1
+        return True
+
+
 if __name__ == '__main__':
-    n = "0P"
-    sol = Solution()
+    n = "A man, a plan, a canal: Panama"
+    sol = Solution2()
     res = sol.isPalindrome(n)
     print(f'Result --> {res}')
