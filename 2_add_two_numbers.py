a421da60a3f1954143459fd71f3a94711eb6e982
Михайло <mykhailov.my@protonmail.com>
2022-06-26 15:50:55 +0300

    better solution for space

--- 2_add_two_numbers.py
+++ 2_add_two_numbers.py
@@ -1,7 +1,7 @@
 """
 Status -> Solved by myself
 
-Time complexity -> O(N + M + log(SUM))
+Time complexity -> O(N + M)
 Space complexity -> O(log N)
 
 Solution
@@ -48,9 +48,40 @@ def build_list(num):
 
     return previous_node
 
+
+class Solution2:
+    # Time complexity O(N + M)
+    # Space complexity O(1)
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        extra = 0
+        l1_node = l1
+        l2_node = l2
+        while l1_node or extra:
+            l1_val = l1_node.val if l1_node else 0
+            l2_val = l2_node.val if l2_node else 0
+            if l1_node:
+                l1_node.val = (l1_val + l2_val + extra) % 10
+            else:
+                l1_node = ListNode((l1_val + l2_val + extra) % 10)
+                last_node.next = l1_node
+            extra = (l1_val + l2_val + extra) // 10
+
+            if l2_node and l1_node.next is None and l2_node.next is not None:
+                l1_node.next = l2_node.next
+                l2_node.next = None
+
+            if l1_node.next is None:
+                last_node = l1_node
+            l1_node = l1_node.next if l1_node else None
+            l2_node = l2_node.next if l2_node else None
+        return l1
+
+
 if __name__ == '__main__':
-    first = build_list(342)
-    second = build_list(465)
-    sol = Solution()
+    first = build_list(0)
+    second = build_list(37)
+    sol = Solution2()
     res = sol.addTwoNumbers(first, second)
-    print(f'Result --> {res}')  
+    res2 = from_list_to_num(res)
+    print(f'Result --> {res2}')
+    print(f'Result --> {res}')
