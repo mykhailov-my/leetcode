0deabfa2ec12c7f6ca6ff9e3f5ed791048ce6ee0
Михайло <mykhailov.my@protonmail.com>
2022-07-25 15:57:51 +0300

    359

--- 359_logger_rate_limiter.py
+++ 359_logger_rate_limiter.py
@@ -1,20 +1,23 @@
 """
-Status -> (Solved by myself, Solved with hint, Solved on review, Implemented with solution)
+Status -> Solved by myself
 
-Time complexity ->
-Space complexity ->
+Time complexity -> O(1)
+Space complexity -> O(unique messages)
 
 Solution
-1.
-2.
-3.
+1. create dict msg -> ts
+2. get existing ts for msg
+3. if no msg -> store msg -> ts to dict, return true
+4. else if ts exist ->
+5. if current ts >= existing ts -> update dict, return True
+6. else -> return False 
 """
 
 class Logger:
     msg_mapper = {}
 
     def __init__(self):
-        pass
+        self.msg_mapper = {}
 
     def shouldPrintMessage(self, timestamp: int, message: str) -> bool:
         existing_ts = self.msg_mapper.get(message)
