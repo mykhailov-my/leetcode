e23759a30db03cd1037c71e87d04d37d666d1d41
Михайло <mykhailov.my@protonmail.com>
2022-08-07 11:25:23 +0300

    287

--- 287_find_duplicate.py
+++ 287_find_duplicate.py
@@ -1,13 +1,18 @@
 """
-Status -> (Solved by myself, Solved with hint, Solved on review, Implemented with solution)
+Status -> Implemented with solution
 
-Time complexity ->
-Space complexity ->
+Time complexity -> O(N)
+Space complexity -> O(1)
 
 Solution
-1.
-2.
-3.
+1.iniate fast and slow pointer as nums[0]
+2. it erate forever
+    3. fast = nums[nums[fast]]
+    4. slow = nums[slow]
+    5 break cycle if slow == fast
+6. put slow at begin (slow = nums[0])
+7. repeat but with same speed to the point where the both will be equal (slow == fast)
+8 return slow or fast
 """
 
 class Solution:
@@ -24,13 +29,33 @@ class Solution:
         return res if res <= n else n
         pass
 
-
+class BestSolution:
+    def findDuplicate(self, nums):
+        # Find the intersection point of the two runners.
+        tortoise = hare = nums[0]
+        print(f'T({tortoise}) H({hare})')
+        while True:
+            tortoise = nums[tortoise]
+            hare = nums[nums[hare]]
+            print(f'T({tortoise}) H({hare})')
+            if tortoise == hare:
+                break
+        
+        # Find the "entrance" to the cycle.
+        tortoise = nums[0]
+        while tortoise != hare:
+            tortoise = nums[tortoise]
+            hare = nums[hare]
+        
+        return hare
 
 if __name__ == '__main__':
     n = [1,3,4,2,2,2]
     n = [2,2,2,2,2]
     n = [1,4,4,2,4]
     n = [3,2,2,2,4]
-    sol = Solution()
+    n = [2,5,9,6,9,3,8,9,7,1]
+    
+    sol = BestSolution()
     res = sol.findDuplicate(n)
     print(f'Result --> {res}')  
