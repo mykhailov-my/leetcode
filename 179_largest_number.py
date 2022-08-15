2cc13283a66c28494b1690fa16aba7e26347b73f
Михайло <mykhailov.my@protonmail.com>
2022-08-15 22:55:46 +0300

    179

--- 179_largest_number.py
+++ 179_largest_number.py
@@ -1,44 +1,38 @@
 """
-Status -> (Solved by myself, Solved with hint, Solved on review, Implemented with solution)
+Status -> Solved with hint
 
-Time complexity ->
-Space complexity ->
+Time complexity -> O(N)
+Space complexity -> O(N)
 
 Solution
-1.
-2.
-3.
+1. Quicksort by custom comparison 
+2. compare by converting two ints to string, concat and convert to int again. compare them
+3. build a string out of list of nums
+4. if first symbol is 0 - return 0 else string
 """
 from functools import cmp_to_key
 
 class Solution:
     def largestNumber(self, nums: list[int]) -> str:
         def cmp(x, y):
-            # divide number to digits
-            # compare first digits -> bigger with bigger digit
-            # 
-            s_x = str(x)
-            s_y = str(y)
-            for i in range(max((len(s_x), len(s_y)))):
-                d_x = int(s_x[i]) if i < len(s_x) else -1
-                d_y = int(s_y[i]) if i < len(s_y) else -1
-                if d_y == d_x:
-                    continue
-                else:
-                    # breakpoint()
-                    return d_y - d_x
-            return 0
+            if int(f'{x}{y}') > int(f'{y}{x}'):
+                return 1
+            elif int(f'{x}{y}') < int(f'{y}{x}'):
+                return -1 
+            else:
+                return 0
 
-        # s = sorted(nums, key=cmp_to_key(cmp))
-        nums.sort(key=cmp_to_key(cmp))
-        print(nums)
-        # [].sort()
+        nums.sort(key=cmp_to_key(cmp), reverse=True)
+        res = ''.join(map(str, nums))
+        return '0' if res[0] == '0' else res
+
 
 
 if __name__ == '__main__':
     n = [3,30,34,5,9] # 9 5 34 3 30
     n = [1, 61, 6, 67] # 67 6 61 1
     # n = [70, 7]
+    n = [0, 0]
     sol = Solution()
     res = sol.largestNumber(n)
     print(f'Result --> {res}')  
