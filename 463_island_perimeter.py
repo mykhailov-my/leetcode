b679cad4c7fbde7ef53a018e7bb578013778ae16
Михайло <mykhailov.my@protonmail.com>
2022-10-19 23:18:12 +0300

    463

--- 463_island_perimeter.py
+++ 463_island_perimeter.py
@@ -53,10 +53,70 @@ class Solution:
         return perimetr
 
 
+class Solution2:
+    """
+    Status -> Solved by myself
+
+    Time complexity -> O(R*C)
+    Space complexity -> O(R*C)
+
+    Solution
+    1. Iteratively find first chunk of land
+    2. Use BFS
+        3. inside queue iteration loop check if there is another peace of land to the right, left, top or bottom.
+        If so -> that is new verticy, follow bfs algorithm (add to queue and hashmap). If no -> increment perimetr variable
+    4. return perimetr
+   
+   
+    Notes 
+    """
+    def get_start_position(self, grid):
+        for i in range(len(grid)):
+            for j in range(len(grid[i])):
+                if grid[i][j] == 1:
+                    return i,j
+        return None, None
+
+    def islandPerimeter(self, grid: list[list[int]]) -> int:
+        r, c = self.get_start_position(grid)
+        if r is None:
+            return 0
+        perimetr = 0
+        queue = [(r,c)]
+        seen = {(r,c)}
+        while queue:
+            r,c = queue.pop()
+
+            if r > 0 and grid[r -1][c] == 1: # top
+                if (r - 1, c) not in seen:
+                    queue.append((r - 1, c))
+                    seen.add((r - 1, c))
+            else:
+                perimetr += 1
+            if r < len(grid) - 1 and grid[r +1][c] == 1: # bottom
+                if (r + 1, c) not in seen:
+                    queue.append((r + 1, c))
+                    seen.add((r + 1, c))
+            else:
+                perimetr += 1
+            if c > 0 and grid[r][c -1] == 1 :  # left
+                if (r, c-1) not in seen:
+                    queue.append((r, c-1))
+                    seen.add((r, c-1))
+            else:
+                perimetr += 1
+            if c < len(grid[r]) - 1 and grid[r][c + 1] == 1:  # right
+                if (r, c + 1) not in seen:
+                    queue.append((r, c + 1))
+                    seen.add((r, c + 1))
+            else:
+                perimetr += 1
+        return perimetr
 
 
 if __name__ == '__main__':
     n = [[0,1,0,0],[1,1,1,0],[0,1,0,0],[1,1,0,0]]
-    sol = Solution()
+    n = [[0]]
+    sol = Solution2()
     res = sol.islandPerimeter(n)
     print(f'Result --> {res}')      
