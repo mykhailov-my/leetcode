879507c2ffd61f0d97f9cfd37e70ac367acc0c93
Михайло <mykhailov.my@protonmail.com>
2022-12-26 15:56:33 +0200

    125 reworked

--- 125_valid_palindrome.py
+++ 125_valid_palindrome.py
@@ -33,8 +33,49 @@ class Solution2:
         return True
 
 
+class Solution3:
+    """
+    Reworked 26.12
+    Status -> Solved by myself
+
+    Tags -> Two pointers
+
+    Time complexity -> O(N)
+    Space complexity -> O(1)
+
+    Solution
+    1. iterate by string from two ends
+    2. skip invalid symbols
+    3. if two valid, lower symbols unequal - > return false
+    4. else true
+
+    LC 
+    time - > 71%
+    memory -> 50%
+    """
+
+    def is_valid_char(self, char):
+        if char.isalnum():
+            return True
+        else:
+            return False
+    def isPalindrome(self, s: str) -> bool:
+        first = 0
+        last = len(s) - 1
+        while first < last:
+            if not self.is_valid_char(s[first]):
+                first += 1
+            elif not self.is_valid_char(s[last]):
+                last -= 1
+            else:
+                if s[first].lower() != s[last].lower():
+                    return False
+                first += 1
+                last -= 1
+        return True
+
 if __name__ == '__main__':
     n = "A man, a plan, a canal: Panama"
-    sol = Solution2()
+    sol = Solution3()
     res = sol.isPalindrome(n)
     print(f'Result --> {res}')
